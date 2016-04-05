660d6196985e71454265fa5b05313c43dca96d3a
gracie lundell and paul hess <gracielundell@gmail.com>
2016-04-05 16:39:43 -0700

    add finishing touch

--- app/components/chamber-detail/component.js
+++ app/components/chamber-detail/component.js
@@ -12,16 +12,15 @@ export default Ember.Component.extend({
       }
       var request = 'http://congress.api.sunlightfoundation.com/committees?chamber=' + params.chamber + '&subcommittee=true&parent_committee_id=' + params.committee_id + '&apikey=8e957463652f4bd4a926700a89aab085';
       // console.log("id: ", id);
-      var _this = this;
+      var self = this;
       return Ember.$.getJSON(request).then(function(responseJSON) {
-        if(_this.showSubcommittees === true) {
-          _this.set('showSubcommittees', false);
-        } else {
-          _this.set('showSubcommittees', true);
-          _this.set('subCommitteeParams', responseJSON.results);
-        }
-        console.log(_this.subCommitteeParams);
-        return responseJSON.results;
+        // if(self.showSubcommittees === true) {
+        //   self.set('showSubcommittees', false);
+        // } else {
+          self.set('showSubcommittees', true);
+          self.set('subCommitteeParams', responseJSON.results);
+        // }
+        // return responseJSON.results;
       });
     }
   }
